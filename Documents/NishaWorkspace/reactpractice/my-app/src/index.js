8ea071d88548bf55109cd15e10979ed2709bdca5
nisha.pandey823@gmail.com <nisha.pandey823@gmail.com>
2018-11-15 19:08:47 +0530

    First commit

---
+++ Documents/NishaWorkspace/reactpractice/my-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+var element = React.createElement('h1', { className: 'greeting' }, 'welcome to react');
+ReactDOM.render(element, document.getElementById(''));
+
+
